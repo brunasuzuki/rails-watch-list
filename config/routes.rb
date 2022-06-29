4a10afc3decf8f01d1f1e646e0a87639ee8d3d0b
bruna suzuki <brunasuzuki12@gmail.com>
2022-06-29 13:57:32 -0700

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, except: %i[destroy edit update] do
-    root to: 'root#index'
-    resources :root, except: :index
-  end
 end
