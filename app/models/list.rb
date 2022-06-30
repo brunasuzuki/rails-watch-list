032638dba3a9a9f50dd24e7abffbb7ca02fb710e
bruna suzuki <brunasuzuki12@gmail.com>
2022-06-29 18:36:19 -0700

    some pseudocode

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,3 +4,5 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 end
+
+# dependent: :destroy (toda vez que eu deletar uma lista especificamente, ele vai nas bookmarks com a lista dessa referencia e apagar).
