28185d2d4c347274c67e151b4ae654e97655aeeb
bruna suzuki <brunasuzuki12@gmail.com>
2022-06-30 11:02:05 -0700

    add dotenv gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# API (tudo que vem dps da ? é a Query,  manda junto com a aquisição a chave de API )
