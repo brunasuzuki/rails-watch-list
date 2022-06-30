ad85c4087f808a7a464fd7d9f36fd269bb5cd196
bruna suzuki <brunasuzuki12@gmail.com>
2022-06-29 18:34:39 -0700

    some pseudocode

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,9 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
+  # uniquiness do movie estendendo  para lista(scope)
   validates :movie_id, uniqueness: { scope: :list_id }
 end
+
+# has_many e belongs_to gera metodos, consegue pegar um filme especifico e associar com o bookmark
+# .movie e vai dar o filme com a referencia
