0e9d33970c440768182537f1aac202c118064561
bruna suzuki <brunasuzuki12@gmail.com>
2022-07-04 09:34:01 -0700

    models pseudocode

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,8 @@ class List < ApplicationRecord
   validates :name, uniqueness: true
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  # a lista tem relacao com o bookmark, pra depois chegar em movies
 end
 
 # dependent: :destroy (toda vez que eu deletar uma lista especificamente, ele vai nas bookmarks com a lista dessa referencia e apagar).
+# has_many e belongs_to geram metodos
