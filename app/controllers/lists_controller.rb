c318782dee27c8107725e1f8977b606623f590d6
bruna suzuki <brunasuzuki12@gmail.com>
2022-07-05 19:05:10 -0700

    show controller view routes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,9 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def show
+    # usa o id para pegar uma lista especifica
+    @list = List.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :list, only: %i[index]
+  root to: 'lists#index'
+  resources :list, only: %i[show]
 end
