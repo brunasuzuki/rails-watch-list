1ac42c82aefe825ae60fbef8b31520326e69ce41
bruna suzuki <brunasuzuki12@gmail.com>
2022-07-05 19:38:01 -0700

    add list index, show, new, create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,8 +13,19 @@ class ListsController < ApplicationController
   end
 
   def create
-
+    # no parametro list vai ter conteudo para criar um nome
+    @list = List.new(list_params)
+    # list_params `e um strong params
+    if @list.save
+      redirect_to @list
+    else
+      render :new
+    end
   end
 
+  private
 
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
